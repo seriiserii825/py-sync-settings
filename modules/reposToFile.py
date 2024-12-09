31e6183a740ced0350c8e5e193363bc1d72c8548
serii burduja <seriiburduja@gmail.com>
2024-12-09 10:04:53 +0200

    feat: local sites exclude

--- modules/reposToFile.py
+++ modules/reposToFile.py
@@ -4,20 +4,6 @@ from rich import print
 
 def reposToFile(file_path):
     os.system(f"rm {file_path}")
-    # print('[green]Finding git repos')
-    # command = f"find ~ -maxdepth 8 -name \".git\" -type d  > {file_path}"
-    # os.system(command)
-    # os.system(f"sed -i '/cache/d' {file_path}")
-    # os.system(f"sed -i '/yay/d' {file_path}")
-    # os.system(f"sed -i '/autoload/d' {file_path}")
-    # os.system(f"sed -i '/oh-my-zsh/d' {file_path}")
-    # os.system(f"sed -i '/powerlevel10k/d' {file_path}")
-    # os.system(f"sed -i '/Trash/d' {file_path}")
-    # os.system(f"sed -i '/advanced-custom-fields-wpcli/d' {file_path}")
-    # os.system(f"sed -i 's/.git//g' {file_path}")
-    # os.system(f"sed -i 's/docker/mysql/g' {file_path}")
-    # os.system(f"bat {file_path}")
-
 
   # Directories to exclude
     exclude_dirs = [
@@ -32,6 +18,7 @@ def reposToFile(file_path):
             "oh-my-zsh",
             ".oh-my-zsh",
             "mysql",
+            "Local Sites"
             ]
 
     # Remove the old file if it exists
