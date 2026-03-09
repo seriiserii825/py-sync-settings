c7753a9ba8c20183f1ce6564c8b1fbea1dbd5f78
serii burduja <seriiburduja@gmail.com>
2026-03-09 09:57:14 +0200

    feat: fixed encrypt decrypt

--- utils/decryptFiles.py
+++ utils/decryptFiles.py
@@ -8,12 +8,16 @@ user = os.getlogin()
 
 def decryptFiles():
     if os.path.isfile(".gpgrc"):
-        print(f"[green]Decrypting files")
+        print("[green]Decrypting files")
         with open(".gpgrc", "r") as file:
             lines = file.readlines()
             for line in lines:
                 line = line.replace("\n", "")
                 print(Panel(f"[blue]decrypt line: {line}"))
                 file_without_gpg = line.replace(".gpg", "")
-                os.system(f"rm {file_without_gpg}")
-                os.system(f"gpg -d {line} > {file_without_gpg}")
+                if os.path.isfile(file_without_gpg):
+                    os.system(f"rm {file_without_gpg}")
+                if os.path.isfile(line):
+                    os.system(f"gpg -d {line} > {file_without_gpg}")
+                else:
+                    print(f"[red]GPG file not found: {line}")
--- utils/encryptFiles.py
+++ utils/encryptFiles.py
@@ -19,17 +19,21 @@ def addToGitIgnore(filename):
 
 def encryptFiles():
     if os.path.isfile(".gpgrc"):
-        print(f"[green]Encrypting files")
+        print("[green]Encrypting files")
         with open(".gpgrc", "r") as file:
             lines = file.readlines()
             for line in lines:
-                line = line.replace("\n", "")
-                print(f"line: {line}")
-                file_without_gpg = line.replace(".gpg", "")
-                print(f"file_without_gpg: {file_without_gpg}")
-                if os.path.isfile(line):
-                    os.system(f"rm {line}")
-                removeFileFromGitCache(file_path=file_without_gpg)
-                addToGitIgnore(file_without_gpg)
-                file_without_gpg = line.replace(".gpg", "")
-                os.system(f"gpg -e -r {user} {file_without_gpg}")
+                try:
+                    line = line.replace("\n", "")
+                    print(f"line: {line}")
+                    file_without_gpg = line.replace(".gpg", "")
+                    print(f"file_without_gpg: {file_without_gpg}")
+                    if os.path.isfile(line):
+                        os.system(f"rm {line}")
+                    removeFileFromGitCache(file_path=file_without_gpg)
+                    addToGitIgnore(file_without_gpg)
+                    file_without_gpg = line.replace(".gpg", "")
+                    os.system(f"gpg -e -r {user} {file_without_gpg}")
+                except Exception as e:
+                    print(f"[red]Error encrypting file: {line}")
+                    print(e)
